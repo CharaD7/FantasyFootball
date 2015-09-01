289148ad5322d59968f8a9c43b24d06fb64ea923
GeekTrainer <chrhar@microsoft.com>
2015-08-31 17:59:21 -0700

    initial creation

---
+++ AuctionTracker/AuctionTracker/Scripts/models.js
@@ -0,0 +1,24 @@
+﻿///// <reference path="knockout-3.3.0.js" />
+
+function team(owner, name) {
+	var self = this;
+	self.owner = ko.observable(owner);
+	self.name = ko.observable(name);
+	self.players = ko.observableArray([]);
+};
+
+function player() {
+	var self = this;
+	self.firstName = ko.observable();
+	self.lastName = ko.observable();
+	self.fullName = ko.computed(function () { return this.firstName + ' ' + this.lastName }, self);
+	self.nflTeam = ko.observable();
+	self.position = ko.observable();
+}
+
+function AppViewModel() {
+	var self = this;
+	self.teams = ko.observableArray([new team('Christopher', 'Backfield Not Included')]);
+}
+
+ko.applyBindings(new AppViewModel());
