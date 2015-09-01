54659ed71ef88a98c6e88feb760766c682c1ba08
Christopher Harrison <chrhar@microsoft.com>
2015-08-31 18:28:36 -0700

    Added bootstrap and began building UI

--- AuctionTracker/AuctionTracker/Scripts/models.js
+++ AuctionTracker/AuctionTracker/Scripts/models.js
@@ -1,24 +1,38 @@
 ﻿///// <reference path="knockout-3.3.0.js" />
 
-function team(owner, name) {
+function team(id, owner, name) {
 	var self = this;
+	self.id = ko.observable(id);
 	self.owner = ko.observable(owner);
 	self.name = ko.observable(name);
 	self.players = ko.observableArray([]);
+	self.totalLeft = ko.computed(function() { return 
+		var totalBid = 0;
+		this.players.forEach(function (player) {
+			totalBid += player.auctionAmount;
+		});
+		return totalBid;
+	}, self);
+	self.maxBid = ko.computed(function () { return 100 - this.players.length + 1 }, self);
 };
 
-function player() {
+function player(id, firstName, lastName, nflTeam, position, auctionAmount) {
 	var self = this;
-	self.firstName = ko.observable();
-	self.lastName = ko.observable();
+	self.id = ko.observable(id);
+	self.firstName = ko.observable(firstName);
+	self.lastName = ko.observable(lastName);
 	self.fullName = ko.computed(function () { return this.firstName + ' ' + this.lastName }, self);
-	self.nflTeam = ko.observable();
-	self.position = ko.observable();
+	self.nflTeam = ko.observable(nflTeam);
+	self.position = ko.observable(position);
+	self.auctionAmount = ko.observable(auctionAmount);
 }
 
 function AppViewModel() {
 	var self = this;
-	self.teams = ko.observableArray([new team('Christopher', 'Backfield Not Included')]);
+	self.teams = ko.observableArray([
+		new team(1, 'jersey', '*Backfield Not Included'),
+		new team(2, 'Brennan', 'Pistol Shrimp All-Stars')
+	]);
 }
 
 ko.applyBindings(new AppViewModel());
