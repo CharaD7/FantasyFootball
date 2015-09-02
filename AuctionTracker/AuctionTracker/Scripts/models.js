ed1bd247e348b2278befc83fd9cdfc2d5454ca7c
Christopher Harrison <chrhar@microsoft.com>
2015-09-01 17:54:40 -0700

    Added player functionality

--- AuctionTracker/AuctionTracker/Scripts/models.js
+++ AuctionTracker/AuctionTracker/Scripts/models.js
@@ -1,4 +1,12 @@
-﻿///// <reference path="knockout-3.3.0.js" />
+﻿/// <reference path="knockout-3.3.0.js" />
+/// <reference path="jquery-2.1.4.js" />
+
+function rosterPosition(position, minNumber) {
+	var self = this;
+	self.position = ko.observable(position);
+	self.number = ko.observable(0);
+	self.minNumber = ko.observable(minNumber);
+}
 
 function team(id, owner, name) {
 	var self = this;
@@ -6,15 +14,33 @@ function team(id, owner, name) {
 	self.owner = ko.observable(owner);
 	self.name = ko.observable(name);
 	self.players = ko.observableArray([]);
-	self.totalLeft = ko.computed(function() {
+	self.rosterPositions = ko.observableArray([
+		new rosterPosition('QB', 1),
+		new rosterPosition('RB', 2),
+		new rosterPosition('WR', 3),
+		new rosterPosition('TE', 1),
+		new rosterPosition('K', 1),
+		new rosterPosition('D', 1),
+	]);
+
+	self.addPlayer = function (player) {
+		self.players.push(player);
+		self.rosterPositions().forEach(function (rosterPosition) {
+			if (rosterPosition.position() == player.position()) {
+				rosterPosition.number(rosterPosition.number() + 1);
+			}
+		})
+	}
+
+	self.totalLeft = ko.pureComputed(function () {
 		var totalBid = 0;
-		alert(this.players().length);
-		this.players().forEach(function (player) {
+		self.players().forEach(function (player) {
 			totalBid += player.auctionAmount();
 		});
-		return totalBid;
-	}, this);
-	self.maxBid = ko.computed(function () { return 100 - this.players.length + 1 }, self);
+		return (100 - totalBid);
+	});
+
+	self.maxBid = ko.pureComputed(function () { return self.totalLeft() - (16 - self.players().length) + 1 });
 };
 
 function player(id, firstName, lastName, nflTeam, position, auctionAmount) {
@@ -30,11 +56,16 @@ function player(id, firstName, lastName, nflTeam, position, auctionAmount) {
 
 function AppViewModel() {
 	var self = this;
+
+	var bni = new team(1, 'jersey', '*Backfield Not Included');
+	bni.addPlayer(new player(1, 'Phillip', 'Rivers', 'Chargers', 'QB', 20));
 	self.teams = ko.observableArray([
-		new team(1, 'jersey', '*Backfield Not Included'),
+		bni,
 		new team(2, 'Brennan', 'Pistol Shrimp All-Stars')
 	]);
-	self.teams()[0].players().push(new player(1, 'Phillip', 'Rivers', 'Chargers', 'QB', 20));
 }
 
-ko.applyBindings(new AppViewModel());
+var appViewModel = new AppViewModel();
+ko.applyBindings(appViewModel);
+appViewModel.teams()[0].addPlayer(new player(2, 'Eli', 'Manning', 'Giants', 'QB', 10));
+appViewModel.teams()[0].addPlayer(new player(3, 'Sam', 'Bradford', 'Eagles', 'QB', 5));
